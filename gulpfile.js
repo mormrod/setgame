45ac63e575f5db3f9c319487b0f4f467bcb79d80
Matt Ormrod <matt@mattormrod.co.uk>
2016-04-02 23:14:12 +0100

    Created the gulp file and built the app structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,35 @@
-/**
- * Created by givesmart on 02/04/16.
- */
+// Load in the dependancies
+var elixir = require('laravel-elixir');
+
+// Setup the paths
+var path = {
+    bower:  "./bower_components/",
+    node:   "./node_modules/",
+    css:    "./app/resources/css/",
+    js:     "./app/resources/js/",
+    destination: "./public/"
+};
+
+// Add the vendor scripts in here, generally from bower_components or node_modules
+var vendorScripts = [
+    path.bower + "jquery/dist/jquery.slim.js",
+    path.bower + "lodash/dist/lodash.js"
+];
+
+// Main task
+elixir(function(mix) {
+
+    // Copy the main html file from the app folder to the public
+    mix.copy("./app/index.html", path.destination + "index.html");
+
+    // Compile the Sass file to the public folder
+    mix.sass(path.css + "main.scss", path.destination + "css/main.css");
+
+    // Concatenate the Vendor scripts
+    mix.scripts(vendorScripts, path.destination + "js/vendor.js");
+
+    // Browserify the main js app
+    mix.browserify(path.js + "app.js", path.destination + "js/app.js");
+
+});
+
