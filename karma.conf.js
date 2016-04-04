283f630a49ab792e20918a8f2bd7883f2d8cdc4b
Matt Ormrod <matt.ormrod@givesmart.com>
2016-04-04 16:01:14 +0100

    Karma coverage update

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
             'tests/*.js' : ['browserify'],
-            'app/resources/js/*.js' : ['browserify', 'coverage']
+            'app/resources/js/*.js' : ['browserify']
         },
 
         browserify : {
@@ -41,18 +41,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'coverage'],
-
-        coverageReporter: {
-            // specify a common output directory
-            dir: 'tests/coverage',
-            reporters: [
-                // reporters not supporting the `file` property
-                { type: 'html', subdir: 'report-html' },
-                { type: 'lcov', subdir: 'report-lcov' },
-                //{ type: 'lcovonly', subdir: '.', file: '../../node_modules/coveralls/bin/coveralls.js' }
-            ]
-        },
+        reporters: ['progress'],
 
         // web server port
         port: 9876,
