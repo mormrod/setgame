3f2d1e0112d61df82134e12918f22e215c9ffbbf
Matt Ormrod <matt@mattormrod.co.uk>
2016-04-04 01:07:54 +0100

    Added Karma jasmine test suite and added first test

---
+++ tests/deckTest.js
@@ -0,0 +1,13 @@
+var deck = require('../app/resources/js/models/deck');
+
+beforeEach(function () {
+
+    deck.initialise(10);
+
+});
+
+it ("should initialise the deck with the given number of cards", function () {
+
+    expect(deck.raw.length === 10).toBe(true);
+
+});
